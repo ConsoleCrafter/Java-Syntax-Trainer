dab0655b94803bb875f34aa5ce1e4a3d07390881
ConsoleCrafter <hmm688907@gmail.com>
2025-10-19 20:55:31 +0200

    Create level6.java

---
+++ level6.java
@@ -0,0 +1,13 @@
+class Spieler {
+    String name;
+    int level;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Spieler s = new Spieler();
+        s.name = "Linus";
+        s.level = 10;
+        System.out.println(s.name + " ist Level " + s.level);
+    }
+}
