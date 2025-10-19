26267616ee306eba9b9dffbfd1c696c60d384781
ConsoleCrafter <hmm688907@gmail.com>
2025-10-19 20:54:25 +0200

    Create level5.java

---
+++ level5.java
@@ -0,0 +1,7 @@
+public static void sendeNachricht(String msg) {
+    System.out.println("[Server] " + msg);
+}
+
+public static void main(String[] args) {
+    sendeNachricht("Willkommen auf dem Server!");
+}
